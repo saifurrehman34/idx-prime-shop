484e8769db2ef4e2c8d84c244271465046b6e021
Saif Ur Rehman <en.saifurrehman01@gmail.com>
2025-06-30 06:28:09 +0000

    not loged in admin enven user it will continusly redirect to login page

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -21,22 +21,16 @@ export const createClient = () => {
           return cookieStore.get(name)?.value;
         },
         set(name: string, value: string, options: CookieOptions) {
-          try {
-            cookieStore.set({ name, value, ...options });
-          } catch (error) {
-            // The `set` method was called from a Server Component.
-            // This can be ignored if you have middleware refreshing
-            // user sessions.
-          }
+          // The `set` method was called from a Server Component.
+          // This can be ignored if you have middleware refreshing
+          // user sessions.
+          cookieStore.set({ name, value, ...options });
         },
         remove(name: string, options: CookieOptions) {
-          try {
-            cookieStore.set({ name, value: '', ...options });
-          } catch (error) {
-            // The `delete` method was called from a Server Component.
-            // This can be ignored if you have middleware refreshing
-            // user sessions.
-          }
+          // The `delete` method was called from a Server Component.
+          // This can be ignored if you have middleware refreshing
+          // user sessions.
+          cookieStore.set({ name, value: '', ...options });
         },
       },
     }
