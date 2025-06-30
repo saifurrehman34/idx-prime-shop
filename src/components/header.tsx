93332a8ed757c0246ff89c86f998fbd8c1c06ef2
Saif Ur Rehman <en.saifurrehman01@gmail.com>
2025-06-30 06:54:14 +0000

    faceing this erro
    1 of 1 error
    Next.js (14.2.3) is outdated (learn mor

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -44,7 +44,7 @@ const ListItem = React.forwardRef<
   return (
     <li>
       <NavigationMenuLink asChild>
-        <a
+        <Link
           ref={ref}
           className={cn(
             "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
@@ -56,7 +56,7 @@ const ListItem = React.forwardRef<
           <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
             {children}
           </p>
-        </a>
+        </Link>
       </NavigationMenuLink>
     </li>
   )
